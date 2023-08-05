b5088fdada9f139f40c4abceceb6eba63ebe5098
Lazy Goose <shamal.ma.rss@gmail.com>
2023-08-05 13:48:54 +0300

    feat: add eslint-config-airbnb/-airbnb-typescript

--- ecommerce-application/.eslintrc.cjs
+++ ecommerce-application/.eslintrc.cjs
@@ -2,12 +2,20 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [

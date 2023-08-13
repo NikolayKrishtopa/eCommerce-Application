90902bc3d09dd5524e2640b37c42cd95b88f30b5
Nikolay Krishtopa <nikolay.krishtopa@gmail.com>
2023-08-13 14:26:21 +0400

    feat: create alert base

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,3 +1,26 @@
+import { useState } from 'react'
+import SystMsgAlert from '../Components/SystMsgAlert/SystMsgAlert'
+
 export default function App() {
-  return <h1>Hello from task crushers</h1>
+  const [systMsg, setSystMsg] = useState('')
+  const [isError, setIsError] = useState(true)
+
+  const resetSystMsg = () => {
+    setSystMsg('')
+    setIsError(false)
+  }
+
+  return (
+    <>
+    <h1 className="title">Welcome from task crushers</h1>
+      <SystMsgAlert
+        msg={systMsg}
+        onResetMsg={resetSystMsg}
+        type={isError ? 'fail' : 'success'}
+      />
+      <button type="button" onClick={() => setSystMsg('test message')}>
+        test
+      </button>
+    </>
+  )
 }

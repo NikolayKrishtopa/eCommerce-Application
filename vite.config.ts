198e12f35b8c05268819327b6eb14114aaabe493
Nikolay Krishtopa <nikolay.krishtopa@gmail.com>
2023-08-13 19:16:49 +0400

    Revert "Optimize svg assets"

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
 /// <reference types="vitest" />
 
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+/**
+ * The use of __dirname and __filename in ES modules is a Vite specific feature
+ */
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      { find: '@', replacement: '/src' },
-      { find: '@public', replacement: '' },
-    ],
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@public': resolve(__dirname, './public'),
+    },
   },
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
-  },
-  css: {
-    devSourcemap: true,
-    modules: {
-      localsConvention: 'camelCaseOnly',
-    },
   },
 })

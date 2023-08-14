19097c471c874a8209499c5b5ef7de0a7e265186
Lazy Goose <shamal.ma.rss@gmail.com>
2023-08-14 08:42:24 +0300

    refactor: rename comonStyles.scss (with typo) to general.scss in styles/

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App/App'
-import './styles/comonStyles.scss'
+import './styles/general.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

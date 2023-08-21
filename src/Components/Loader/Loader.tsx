0abcc8210c22f6225b3b7e0d05269aec134e1db9
Lazy Goose <shamal.ma.rss@gmail.com>
2023-08-21 11:27:35 +0300

    feat: impl Loader and FullScreenLoader components

---
+++ src/Components/Loader/Loader.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react'
+import cn from 'classnames'
+import s from './Loader.module.scss'
+
+type LoaderProps = Partial<{
+  show: boolean
+  className: string
+}>
+
+export default function Loader(props: LoaderProps) {
+  const { show = true, className = '' } = props
+
+  return <div className={cn(s.loader, { [s.hide]: !show }, className)} />
+}
+
+export function FullPageLoader(props: LoaderProps) {
+  const { show = true, className = '' } = props
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden'
+    return () => {
+      document.body.style.overflow = 'unset'
+    }
+  }, [])
+
+  return (
+    <div className={cn(s.fullPageMask, { [s.hide]: !show }, className)}>
+      <div className={cn(s.loader)} />
+    </div>
+  )
+}

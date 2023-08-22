f46b54eace092f1fcf6c76ee59ffeff06ba98263
Lazy Goose <shamal.ma.rss@gmail.com>
2023-08-22 12:05:12 +0300

    feat(test:auth): validate password

--- src/Pages/AuthPage/validateForm.test.ts
+++ src/Pages/AuthPage/validateForm.test.ts
@@ -95,4 +95,54 @@ describe('Registration form validation', () => {
       testError('email', expectError(true))
     })
   })
+
+  describe('Password validation', () => {
+    const expectError = expectFormErrorMaker('password')
+
+    describe('Password must be at least 8 characters long', () => {
+      testValid('123456Aa9', expectError(false))
+      testError('aA0#5678', expectError(true))
+      describe('With special symbol:', () => {
+        testValid('aA0$56789', expectError(false))
+      })
+    })
+    describe('Password must contain at least one uppercase letter (A-Z)', () => {
+      testValid(`${'A'}a0b56789`, expectError(false))
+      testError(`${'a'}a0b56789`, expectError(true))
+      describe('With special symbol:', () => {
+        testValid(`${'A'}a0#56789`, expectError(false))
+      })
+    })
+    describe('Password must contain at least one lowercase letter (a-z)', () => {
+      testValid(`${'a'}ABC06789`, expectError(false))
+      testError(`${'A'}ABC06789`, expectError(true))
+      describe('With special symbol:', () => {
+        testValid(`${'a'}ABC0#789`, expectError(false))
+      })
+    })
+    describe('Password must contain at least one digit (0-9)', () => {
+      testValid(`${'1'}bAaBbCc`, expectError(false))
+      testError(`${'d'}bAaBbCc`, expectError(true))
+      describe('With special symbol:', () => {
+        testValid(`${'1'}bA#BbCc`, expectError(false))
+      })
+    })
+    describe('(Optional) Password must contain at least one special character (e.g., !@#$%^&*)', () => {
+      testValid(`${'!'}aABbCc89`, expectError(false))
+      testValid(`${'@'}aABbCc89`, expectError(false))
+      testValid(`${'#'}aABbCc89`, expectError(false))
+      testValid(`${'$'}aABbCc89`, expectError(false))
+      testValid(`${'%'}aABbCc89`, expectError(false))
+      testValid(`${'^'}aABbCc89`, expectError(false))
+      testValid(`${'&'}aABbCc89`, expectError(false))
+      testValid(`${'*'}aABbCc89`, expectError(false))
+      testValid(`${')'}aABbCc89`, expectError(false))
+      testError(`${'d'}aABbCc89`, expectError(true))
+    })
+    describe('Password must not contain leading or trailing whitespace', () => {
+      testError(`${' '}aABbCc`, expectError(true))
+      testError(`aABbCc${' '}`, expectError(true))
+      testError(`${' '}aABbCc${' '}`, expectError(true))
+    })
+  })
 })

5d80f40b22c213413702688792d49f6fd31882fc
Lazy Goose <shamal.ma.rss@gmail.com>
2023-09-04 21:07:34 +0300

    feat(ui): impl type="date" within <TextInput />

--- src/Components/UIKit/TextInput/TextInput.props.ts
+++ src/Components/UIKit/TextInput/TextInput.props.ts
@@ -2,7 +2,7 @@ import InputSlotProps from '../InputSlot/InputSlot.props'
 
 type TextInputProps = JSX.IntrinsicElements['input'] &
   Omit<InputSlotProps, 'htmlFor'> & {
-    type?: 'text' | 'number' | 'email' | 'password'
+    type?: 'text' | 'number' | 'email' | 'password' | 'date'
     id?: string | number
   }
 

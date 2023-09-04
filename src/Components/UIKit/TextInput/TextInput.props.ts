49fe7e421655b5d84da32958b2d8eb71bfb63359
Lazy Goose <shamal.ma.rss@gmail.com>
2023-09-04 10:08:48 +0300

    refactor(ui): rework <TextInput /> using <InputSlot />

--- src/Components/UIKit/TextInput/TextInput.props.ts
+++ src/Components/UIKit/TextInput/TextInput.props.ts
@@ -1,13 +1,9 @@
-type TextInputProps = JSX.IntrinsicElements['input'] & {
-  type?: 'text' | 'email' | 'password' | 'date'
-  id?: string | number
-  label?: string
-  error?: string
-  errorJump?: boolean
-  icon?: JSX.Element
-  iconError?: JSX.Element
-  children?: JSX.Element
-  onIconClick?: () => void
-}
+import InputSlotProps from '../InputSlot/InputSlot.props'
+
+type TextInputProps = JSX.IntrinsicElements['input'] &
+  Omit<InputSlotProps, 'htmlFor'> & {
+    type?: 'text' | 'number' | 'email' | 'password'
+    id?: string | number
+  }
 
 export default TextInputProps
--- src/Components/UIKit/TextInput/TextInput.tsx
+++ src/Components/UIKit/TextInput/TextInput.tsx
@@ -1,6 +1,6 @@
-import cn from 'classnames'
-import { ReactComponent as SvgError } from '@/assets/icons/error.svg'
-import styleVars from './TextInput.module.scss'
+import './TextInput.scss'
+import useMemoId from '@/hooks/useMemoId'
+import InputSlot from '../InputSlot/InputSlot'
 import TextInputProps from './TextInput.props'
 
 export default function TextInput(props: TextInputProps) {
@@ -10,7 +10,7 @@ export default function TextInput(props: TextInputProps) {
     label,
     id,
     error,
-    errorJump = false,
+    errorJump,
     icon,
     iconError,
     onIconClick,
@@ -19,74 +19,28 @@ export default function TextInput(props: TextInputProps) {
     ...inputProps
   } = props
 
-  const isError = typeof error === 'string' && error.length
-
-  let inputId: string | undefined
-
-  if (typeof id === 'number') {
-    inputId = String(id)
-  }
-
-  if (label) {
-    inputId = id || crypto.randomUUID()
-  }
+  const inputId = useMemoId(id)
 
   return (
-    <div
-      className={cn(
-        'text-input',
-        {
-          'text-input--error': isError,
-          'text-input--disabled': disabled,
-        },
-        className,
-      )}
+    <InputSlot
+      htmlFor={inputId}
+      className={className}
+      label={label}
+      error={error}
+      errorJump={errorJump}
+      icon={icon}
+      iconError={iconError}
+      onIconClick={onIconClick}
+      disabled={disabled}
     >
-      {label && (
-        <label htmlFor={inputId} className="text-input__label">
-          {label}
-        </label>
-      )}
-      <div
-        className={cn('text-input__input-box', {
-          'text-input__input-box--icon': icon,
-        })}
-      >
-        <input
-          id={inputId}
-          type={type}
-          className="text-input__input"
-          disabled={disabled}
-          /* eslint-disable-next-line react/jsx-props-no-spreading */
-          {...inputProps}
-        />
-        {icon && (
-          <button
-            type="button"
-            className="text-input__icon-btn"
-            onClick={onIconClick}
-            disabled={disabled || !onIconClick}
-          >
-            {isError ? iconError || icon : icon}
-          </button>
-        )}
-      </div>
-      {(error || !errorJump) && (
-        <div className="text-input__error">
-          {error && (
-            <>
-              <SvgError
-                className="text-input__error-icon"
-                fill="currentColor"
-              />
-              <p className="text-input__error-msg">{error}</p>
-            </>
-          )}
-        </div>
-      )}
-    </div>
+      <input
+        className="text-input"
+        type={type}
+        id={inputId}
+        disabled={disabled}
+        /* eslint-disable-next-line react/jsx-props-no-spreading */
+        {...inputProps}
+      />
+    </InputSlot>
   )
 }
-
-TextInput.ColorError = styleVars.colorDanger
-TextInput.ColorMain = styleVars.colorMain

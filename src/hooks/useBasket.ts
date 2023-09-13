34469c42ca754122b212c42b8c4230be9d776f34
Lazy Goose <shamal.ma.rss@gmail.com>
2023-09-13 16:16:46 +0300

    feat(use-basket): add/remove/updateQuantity cart item

--- src/hooks/useBasket.ts
+++ src/hooks/useBasket.ts
@@ -1,6 +1,6 @@
 import { apiRoot } from '@/eComMerchant/client'
-import { Cart } from '@commercetools/platform-sdk'
-import { useEffect, useState } from 'react'
+import { Cart, CartUpdateAction } from '@commercetools/platform-sdk'
+import { useEffect, useRef, useState } from 'react'
 
 const STORAGE_BASKET_ID = 'basketId'
 const DEFAULT_CURRENCY = 'EUR'
@@ -8,7 +8,13 @@ const DEFAULT_CURRENCY = 'EUR'
 export default function useBasket() {
   const basketId = localStorage.getItem(STORAGE_BASKET_ID)
 
+  const basketRef = useRef<Cart>()
   const [basket, setBasket] = useState<Cart>()
+
+  const updateBasket = (cart: Cart) => {
+    setBasket(cart)
+    basketRef.current = cart
+  }
 
   const [isFetching, setIsFetching] = useState(true)
   const [isLoaded, setIsLoaded] = useState(false)
@@ -24,7 +30,7 @@ export default function useBasket() {
           .then((resp) => {
             const { statusCode, body } = resp
             if (statusCode === 200) {
-              setBasket(body)
+              updateBasket(body)
             }
           })
       } else {
@@ -35,7 +41,7 @@ export default function useBasket() {
           .then((resp) => {
             const { statusCode, body } = resp
             if (statusCode === 201) {
-              setBasket(body)
+              updateBasket(body)
               localStorage.setItem(STORAGE_BASKET_ID, body.id)
             }
           })
@@ -46,9 +52,84 @@ export default function useBasket() {
     }
   }, [basketId])
 
+  const updateCart = (cartUpdate: CartUpdateAction | CartUpdateAction[]) => {
+    if (!basketId) {
+      throw new Error('Basket is not created')
+    }
+    if (!basketRef.current) {
+      throw new Error('Basket is not fetched yet')
+    }
+    return apiRoot
+      .carts()
+      .withId({ ID: basketId })
+      .post({
+        body: {
+          version: basketRef.current.version,
+          actions: [cartUpdate].flat(),
+        },
+      })
+      .execute()
+      .then((resp) => {
+        const { statusCode, body } = resp
+        if (statusCode === 200) {
+          updateBasket(body)
+          localStorage.setItem(STORAGE_BASKET_ID, body.id)
+        }
+        return resp
+      })
+  }
+
+  const findLineItemBy = ({ productId }: { productId: string }) =>
+    basketRef.current?.lineItems.find((li) => li.productId === productId)
+
+  /* Expose API */
+
+  const addItem = async (productId: string, quantity = 1) => {
+    setIsFetching(true)
+    try {
+      await updateCart({ action: 'addLineItem', productId, quantity })
+    } finally {
+      setIsFetching(false)
+    }
+  }
+
+  const removeItem = async (productId: string) => {
+    const lineItem = findLineItemBy({ productId })
+    if (lineItem) {
+      setIsFetching(true)
+      try {
+        await updateCart({ action: 'removeLineItem', lineItemId: lineItem.id })
+      } finally {
+        setIsFetching(false)
+      }
+    }
+  }
+
+  const updateItemQuantity = async (
+    productId: string,
+    updater: (q: number) => number,
+  ) => {
+    const lineItem = findLineItemBy({ productId })
+    if (lineItem) {
+      setIsFetching(true)
+      try {
+        await updateCart({
+          action: 'changeLineItemQuantity',
+          lineItemId: lineItem.id,
+          quantity: updater(lineItem.quantity),
+        })
+      } finally {
+        setIsFetching(false)
+      }
+    }
+  }
+
   return {
     basket,
     isLoaded,
     isFetching,
+    addItem,
+    removeItem,
+    updateItemQuantity,
   }
 }

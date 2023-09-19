817ea5cf732f3d263df816a19057ca2a111f395d
Lazy Goose <shamal.ma.rss@gmail.com>
2023-09-19 11:39:40 +0300

    feat(use-auth): impl initial authentication on hook load

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,3 +1,11 @@
+import {
+  buildClientCredentialsFlowApiRoot,
+  buildRefreshTokenFlowApiRoot,
+  buildAnonymousSessionFlowApiRoot,
+} from '@/client/client'
+import { type Cart, type Customer } from '@commercetools/platform-sdk'
+import { useEffect, useRef, useState } from 'react'
+
 enum SessionType {
   Client = 'client',
   Anonymous = 'anonymous',
@@ -50,10 +58,98 @@ class SessionStore {
   }
 }
 
-const sessionStore = new SessionStore()
-sessionStore.retrieveFromLocalStorage()
+const sessionStore = new SessionStore().retrieveFromLocalStorage()
 
-function useAuth() {}
+function useAuth() {
+  const [currentUser, setCurrentUser] = useState<Customer>()
+  const [currentCart, setCurrentCart] = useState<Cart>()
+
+  const authApiRootRef = useRef(buildClientCredentialsFlowApiRoot())
+
+  const validateToken = async (token: string) =>
+    authApiRootRef.current.validateRefreshToken(token)
+
+  const authenticateClientApp = () => {
+    setCurrentUser(undefined)
+    authApiRootRef.current = buildClientCredentialsFlowApiRoot()
+    sessionStore.set(SessionType.Client)
+  }
+
+  const authenticateAnonymous = async (refreshToken?: string) => {
+    try {
+      if (refreshToken) {
+        const apiRoot = buildRefreshTokenFlowApiRoot(refreshToken)
+        setCurrentUser(undefined)
+        authApiRootRef.current = apiRoot
+      } else {
+        const apiRoot = buildAnonymousSessionFlowApiRoot()
+        const token = await apiRoot.retrieveToken()
+        sessionStore.set(SessionType.Anonymous, token.refreshToken)
+      }
+    } catch {
+      authenticateClientApp()
+    }
+  }
+
+  const authenticateFallback = () => authenticateClientApp()
+
+  const authenticateCustomer = async (refreshToken: string) => {
+    try {
+      const apiRoot = buildRefreshTokenFlowApiRoot(refreshToken)
+      const response = await apiRoot.me().get().execute()
+      if (response.statusCode === 200) {
+        setCurrentUser(response.body)
+        sessionStore.set(SessionType.Authenticated, refreshToken)
+        authApiRootRef.current = apiRoot
+      } else {
+        throw new Error('Jump into catch')
+      }
+    } catch {
+      authenticateFallback()
+    }
+  }
+
+  const onTokenValidation = (
+    token: string,
+    onValid: (t: string) => unknown,
+    onInvalid: (t: string, e?: Error) => unknown = authenticateFallback,
+  ) =>
+    validateToken(token)
+      .then((isValid) => {
+        if (isValid) onValid(token)
+        else onInvalid(token)
+      })
+      .catch((e) => onInvalid(token, e))
+
+  useEffect(() => {
+    const { session, refreshToken } = sessionStore.get()
+    if (session === SessionType.Authenticated) {
+      onTokenValidation(refreshToken, authenticateCustomer)
+      return
+    }
+    if (session === SessionType.Anonymous) {
+      onTokenValidation(refreshToken, authenticateAnonymous)
+      return
+    }
+    authenticateFallback()
+  }, [])
+
+  const authApiRoot = () => authApiRootRef.current
+
+  const sessionState = sessionStore.get().session
+  const isAuthenticated =
+    sessionStore.get().session === SessionType.Authenticated
+
+  return {
+    authApiRoot,
+    sessionState,
+    isAuthenticated,
+    currentUser,
+    currentCart,
+    setCurrentUser,
+    setCurrentCart,
+  }
+}
 
 export default useAuth
 export { SessionType }
